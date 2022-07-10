a80479ba949a98c2acf9da6b3b90c6b3097fc827
Rohitnyati <108011275+Rohitnyati@users.noreply.github.com>
2022-07-10 19:43:17 +0530

    Update Example5.py

--- More Questions/Example5.py
+++ More Questions/Example5.py
@@ -1,3 +1,22 @@
+"""Print the following inverted staircase pattern based on input size:
+Input:
+N = 9
+Output:
+9 8 7 6 5 4 3 2 1
+8 7 6 5 4 3 2 1
+6 5 4 3 2 1
+3 2 1
+0
+Input:
+N = 10
+Output:
+10 9 8 7 6 5 4 3 2 1
+9 8 7 6 5 4 3 2 1
+7 6 5 4 3 2 1
+4 3 2 1
+0
+"""
+
 def pattern(n):
     m=5
     for i in range(0,m):
